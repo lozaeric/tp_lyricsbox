64834a930b361419d6706ce3f5b7706f5e070a95
mat105 <mat.105@hotmail.com>
2016-05-12 15:28:01 -0300

    actualizado usuario

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -8,17 +8,37 @@
 
 
 		public function get_usuarios ($nMejores=10) {
-            $datos = $this->db->get('USUARIO')->result_array();
-            return $datos;
+            $this->db->select('*');
+            $this->db->from('USUARIO');
+            $this->db->order_by('puntos');
+            $this->db->limit(10);
+            
+            return $this->db->get()->result_array();
 		}
 		
 		public function get_usuario ($id) {
+            $this->db->select('*');
+            $this->db->from('USUARIO');
+            $this->db->where('codigo' $id);
+            return $query->row_array();
 		}
 		
 		public function get_canciones ($id) {
+            $this->db->select('codigo, nombre, anio, disco, artista');
+            $this->db->from('CANCION');
+            $this->db->join('USUARIO', 'USUARIO.codigo = CANCION.codigo');
+            $this->db->where('USUARIO.codigo' $id);
+            
+            return $query->result_array();
 		}
 		
 		public function get_juegos ($id) {
+            $this->db->select('codigo_fragmento, tiempo, dificultad, puntaje');
+            $this->db->from('JUEGO');
+            $this->db->join('USUARIO', 'USUARIO.codigo = JUEGO.codigo_usuario');
+            $this->db->where('USUARIO.codigo', $id);
+            
+            return $query->result_array();
 		}		
 	}
 
